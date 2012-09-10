c6f77306b8ef75334c0b244c9f336a22c7ad0c31
Alen Siljak <alen.siljak@gmail.com>
2012-09-10 13:26:31 +0200

    working on handshake handler

--- backend/js/src/modules/handshake.js
+++ backend/js/src/modules/handshake.js
@@ -4,13 +4,15 @@
 
 exports.handle = function(req, res){
 	var method = req.param('method', null);
-	var parameters = req.param('parameters', null);
+	var parametersString = req.param('parameters', null);
+	var parametersContainer = JSON.parse(parametersString);
+	var parameters = parametersContainer.parameters;
 
 	var message = parameters['message'];
 
 	switch(message)	{
 		case "connect":
-
+			connect(req.session, parameters);
 		break;
 
 		case "credentialCheck":
@@ -27,6 +29,13 @@ exports.handle = function(req, res){
 	res.send('alles klar');
 };
 
-var connect = function(){
-	
+var connect = function(session, parameters){
+
+	session.C = parameters['parameters']['C']
+	session.A = parameters['parameters']['A']
+	// console.log("session.C = " + session.C);
+
+	// var user = db.Query(User).filter('username =', session.C).get()
+	var user = { "username": "user" };
+
 };
